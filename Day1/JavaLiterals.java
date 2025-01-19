f389c8fd46853564eff0f559bc69f9c53bee7523
Praneet2003 <rajpraneet4@gmail.com>
2025-01-19 11:41:44 +0530

    Java literals

---
+++ Day1/JavaLiterals.java
@@ -0,0 +1,17 @@
+class JavaLiterals{
+    public static void main(String arr[]){
+        byte age = 34;//range is from -128 to 127
+        int a = 48;
+        long l = 56778738;
+        char ch = 'B';
+        //it is necessary to add f at literals of float. 
+        float f1 = 5.72f;
+        double d1 = 5.67867652;//by default decimal point number are in double format in java.
+        System.out.print("Byte data: "+age);
+        System.out.print("\nInt data: "+a);
+        System.out.print("\nFLoat Data: "+f1);
+        System.out.print("\nLong data: "+l);
+        System.out.print("\nChar Data: "+ch);
+        System.out.print("\nDouble data: "+d1);
+    }
+}
