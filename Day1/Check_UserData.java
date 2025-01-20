78d084e5c33ee970e3dd34a59c4d6cf653235afd
Praneet2003 <rajpraneet4@gmail.com>
2025-01-20 21:56:43 +0530

    Checking whether data enterd by user is correct or not

---
+++ Day1/Check_UserData.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Check_UserData{
+    public static void main(String arr[]){
+        Scanner sc = new Scanner(System.in);
+        boolean b = sc.hasNextInt();
+        System.out.print("The number Entered is: "+b);
+    }
+}
+// 56
+// The number Entered is: true
+// 89.0
+// The number Entered is: false
