f6bb8da60e592d3c82677603eb95cf6e3087a9b9
Praneet2003 <rajpraneet4@gmail.com>
2025-02-01 21:27:36 +0530

    Constructor Overloading

---
+++ Day11/Constructor_Overloading.java
@@ -0,0 +1,45 @@
+class Employee{
+    private int id;
+    private String name;
+    // here the constructor is overloaded.
+    // one is having parameters and other does not.
+    public Employee(String n, int x){
+        System.out.println("Hi I am constructor having Two parameters.");
+        id = x;
+        name = n;
+    }
+    public Employee(){
+        System.out.println("Hi I am constructor having no parameters.");
+        id =101;
+        name = "Praneet Raj";
+    }
+    public Employee(int x){
+        System.out.println("Hi I am constructor having One parameters i.e ID.");
+        id = x;
+        name = "Ayush Gupta";
+    }
+    public Employee(String n){
+        System.out.println("Hi I am constructor having One parameters i.e Name.");
+        id = 78;
+        name = n;
+    }
+    public void getDetails(){
+        System.out.println("Name is: "+name+"\nid is: "+id);
+    }
+}
+public class Constructor_Overloading{
+    public static void main(String args[]){
+        Employee E = new Employee();
+        //object created without passing any arguments.
+        E.getDetails();
+        Employee em = new Employee("Mehul raj",45);
+        //object created passing two arguments.
+        em.getDetails();
+        Employee E1 = new Employee(34);
+        //object created passing one argument as id.
+        E1.getDetails();
+        Employee E2 = new Employee("Babalu kumar");
+        //object created passing one arguments as name.
+        E2.getDetails();
+    }
+}
