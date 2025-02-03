4e372b5a3f48e750c1262f3da869c30fd3f2be1f
Praneet2003 <rajpraneet4@gmail.com>
2025-02-03 21:29:23 +0530

    Last stone weight, problem no 1046

---
+++ LeetCode/lastStone_weight.java
@@ -0,0 +1,60 @@
+
+// You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+// We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+// If x == y, both stones are destroyed, and
+// If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+// At the end of the game, there is at most one stone left.
+
+// Return the weight of the last remaining stone. If there are no stones left, return 0.
+// Example 1:
+
+// Input: stones = [2,7,4,1,8,1]
+// Output: 1
+// Explanation: 
+// We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+// we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+// we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+// we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+// Example 2:
+
+// Input: stones = [1]
+// Output: 1
+// Constraints:
+
+// 1 <= stones.length <= 30
+// 1 <= stones[i] <= 1000
+import java.util.*;
+class Solution{
+    // problem no: 1046.
+    public int lastStoneWeight(int[] stones) {
+        int n= stones.length;
+        while(n>1){
+            Arrays.sort(stones);
+            int stone1 = stones[n-1];
+            int stone2 = stones[n-2];
+            if(stone1==stone2){
+                n=n-2;
+            }else{
+                stones[n-2] = stone1-stone2;
+                n--;
+            }
+        }
+        return n==1? stones[0]:0;
+    }
+}
+public class lastStone_weight{
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of elements to insert in stone array: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the no of elements of stone array: ");
+        int arr[] = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.err.println("The weight of last stone is: "+s.lastStoneWeight(arr));
+    }
+}
