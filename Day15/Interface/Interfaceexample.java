54c36922692cfc8ee67f127b57fdbe9e20efc17f
Praneet2003 <rajpraneet4@gmail.com>
2025-03-23 21:47:35 +0530

    Interface in java

---
+++ Day15/Interface/Interfaceexample.java
@@ -0,0 +1,17 @@
+interface Interface {
+    default void show() {
+        System.out.println("Default Show Method");
+    }
+    
+    static void display() {
+        System.out.println("Static Display Method");
+    }
+}
+
+public class Interfaceexample implements Interface{
+    public static void main(String[] args) {
+        Interfaceexample obj = new Interfaceexample();
+        obj.show();
+        Interface.display();
+    }
+}
