0365b8970f5e71f43733681e06cfc3433053e51a
Praneet2003 <rajpraneet4@gmail.com>
2025-04-27 22:10:21 +0530

    Multiple template in generic class

---
+++ Generic classes/multiple_templates.java
@@ -0,0 +1,19 @@
+class Box<T1, T2>{
+    private T1 a;
+    private T2 b;
+    public T1 get(T1 a){
+        this.a = a;
+        return a;
+    }
+
+}
+class multiple_templates{
+    public static void main(String[] args){
+        Box<String, Integer> StringBox = new Box<>();
+        // StringBox.set("Hello Generics");
+        System.out.println("String Value: "+StringBox.get("praneet"));
+        Box<Integer,String> intBox = new Box<>();
+        // intBox.set(123);
+        System.out.println("Integer Value: "+intBox.get(143));
+    }
+}
