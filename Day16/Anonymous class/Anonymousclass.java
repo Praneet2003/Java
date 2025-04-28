7157f34e77a7c16452aa47bb4925e6309b4f9eab
Praneet2003 <rajpraneet4@gmail.com>
2025-04-28 21:09:34 +0530

    Anonymous class called from outer main function

---
+++ Day16/Anonymous class/Anonymousclass.java
@@ -0,0 +1,26 @@
+// Anannomous classes are those classes which are defined and implemented in same line
+interface A{
+    void seek();
+}
+class outer{
+    void call(){
+        A obj = new A(){
+            public void seek(){//here we are not creating the actual object for the interface but it seems to be an object.
+                System.out.println("Hello World");
+            }
+        };
+        obj.seek();
+    }
+}
+class Anonymousclass{
+    public static void main(String[] args) {
+        A obj = new A(){
+            public void seek(){
+                System.out.println("Hello");
+            }
+        };
+        obj.seek();
+        outer objclass = new outer();
+        objclass.call();
+    }
+}
