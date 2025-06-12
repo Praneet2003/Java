cfe2bbd6d1c3980cdbc8f0e36aed8622d326111e
Praneet2003 <rajpraneet4@gmail.com>
2025-06-12 18:57:29 +0530

    Multiple interface implemented by one cladd

---
+++ Day15/Interface/muliple_interface.java
@@ -0,0 +1,41 @@
+interface Vehicle{
+    void start();
+    void type();
+    void company();
+}
+interface Properties{
+    void SportsMode();
+    void fourWheelDrive();
+}
+class Safari implements Vehicle,Properties{
+    @Override
+    public void start(){
+        System.out.println("The car is started by push start/stop Button.");
+    }
+    @Override
+    public void type(){
+        System.out.println("This has a SUV type Body.");
+    }
+    @Override
+    public void company(){
+        System.out.println("Manufactured by TATA Motors.");
+    }
+    @Override
+    public void SportsMode(){
+        System.out.println("Sports Mode is Engaged.");
+    }
+    @Override
+    public void fourWheelDrive(){
+        System.err.println("4 X 4 Engaged.");
+    }
+}
+class muliple_interface{
+    public static void main(String[] args) {
+        Safari obj = new Safari();
+        obj.company();
+        obj.type();
+        obj.start();
+        obj.SportsMode();
+        obj.fourWheelDrive();
+    }
+}
