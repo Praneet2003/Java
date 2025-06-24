a515cf25b1e59cea524449d086d96b4dec94fae3
Praneet2003 <rajpraneet4@gmail.com>
2025-06-24 16:14:56 +0530

    We cannont use acess modifiers with local VAriable

---
+++ SUmmer traning LPU/Variables/BrainStroming/varDemo.java
@@ -0,0 +1,11 @@
+class varDemo{
+    public static void main(String[] args) {
+        // public int x;//no Acess modifier allowed with local variable.
+        int x;
+        x = 10;
+        System.out.println(x);
+    }
+}
+//Output:
+// Compilation error
+// beause you cannot declare a local varable using any acess modifiers.
