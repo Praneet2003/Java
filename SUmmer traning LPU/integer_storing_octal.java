962746819bd4dd04a6c8beaca1822492162012f8
Praneet2003 <rajpraneet4@gmail.com>
2025-06-29 18:42:27 +0530

    Storing Octal and HexaDecimal in int variable

---
+++ SUmmer traning LPU/integer_storing_octal.java
@@ -0,0 +1,10 @@
+class integer_storing_octal{
+    public static void main(String[] args) {
+        int x = 027;
+        int y = 0XA1;
+        int z = 0XD1;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
