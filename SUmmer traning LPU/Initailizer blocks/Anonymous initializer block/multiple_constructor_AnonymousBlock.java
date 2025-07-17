89471614705c18c01e4a43869eb840ce6ee914cc
Praneet2003 <rajpraneet4@gmail.com>
2025-07-17 14:07:54 +0530

    Annonymous block with Multiple constructor(Overloaded).

---
+++ SUmmer traning LPU/Initailizer blocks/Anonymous initializer block/multiple_constructor_AnonymousBlock.java
@@ -0,0 +1,19 @@
+class multiple_constructor_AnonymousBlock{
+    public multiple_constructor_AnonymousBlock(){
+        System.out.println("First Constructor");
+    }
+    public multiple_constructor_AnonymousBlock(int x){
+        System.out.println("Second Constructor");
+    }
+    {
+        System.out.println("Hello i am also an Anonymous block");
+    }
+    {
+        System.out.print("Hello world i am with the ");
+    }
+    public static void main(String[] args) {
+        multiple_constructor_AnonymousBlock obj = new multiple_constructor_AnonymousBlock();
+        multiple_constructor_AnonymousBlock obj1 = new multiple_constructor_AnonymousBlock(1);
+    }
+
+}
